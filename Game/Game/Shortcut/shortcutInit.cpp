0d717b6c1c34da0db92786ca8c377466623988fb
Dron231 <a.igo@g.nsu.ru>
2025-04-11 11:21:16 +0700

    Упростил shortcutInit, чтобы он рисовал треугольник

--- Game/Game/Shortcut/shortcutInit.cpp
+++ Game/Game/Shortcut/shortcutInit.cpp
@@ -21,7 +21,7 @@ namespace {
 		//normal
 		float nx, ny, nz;
 		//tex
-		float u, v;
+		/*float u, v;*/
 	};
 
 	Shader* shaderTranslated;
@@ -69,18 +69,18 @@ void initGraphics()
 
 	VertexXYZ triangle[] =
 	{
-		VertexXYZ{ 5.0, -5.0, 0.0,  0.0, 0.0, 0.0,  0.0, 1.0 },
-		VertexXYZ{-5.0, -5.0, 0.0,  0.0, 0.0, 0.0,  0.0, 1.0 },
-		VertexXYZ{ 0.0,  5.0, 0.0,  0.0, 0.0, 0.0,  0.0, 1.0 },
-		VertexXYZ{ 5.0, -5.0, 0.0,  0.0, 0.0, 0.0,  0.0, 1.0 },
-		VertexXYZ{-5.0, -5.0, 0.0,  0.0, 0.0, 0.0,  0.0, 1.0 },
-		VertexXYZ{ 0.0,  5.0, 0.0,  0.0, 0.0, 0.0,  0.0, 1.0 }
+		VertexXYZ{ -0.3, -0.2, 0.0,  1.0, 0.0, 0.0/*,  0.0, 1.0*/ },
+		VertexXYZ{  0.0,  0.4, 0.0,  0.0, 1.0, 0.0/*,  0.0, 1.0*/ },
+		VertexXYZ{  0.3, -0.2, 0.0,  0.0, 0.0, 1.0/*,  0.0, 1.0*/ }//,
+		//VertexXYZ{ 0.5, -0.5, 0.0,  0.0, 0.0, 0.0/*,  0.0, 1.0*/ },
+		//VertexXYZ{-0.5, -0.5, 0.0,  0.0, 0.0, 0.0/*,  0.0, 1.0*/ },
+		//VertexXYZ{ 0.0,  0.5, 0.0,  0.0, 0.0, 0.0/*,  0.0, 1.0*/ }
 	};
 	std::vector<VertexElement> elements =
 	{
 		VertexElement{3, GL_FLOAT, false, ElementType::XYZ},
-		VertexElement{3, GL_FLOAT, false, ElementType::NORMAL},
-		VertexElement{2, GL_FLOAT, false, ElementType::UV}
+		VertexElement{3, GL_FLOAT, false, ElementType::NORMAL}//,
+		//VertexElement{2, GL_FLOAT, false, ElementType::UV}
 	};
 
 	VertexSpecification spec(elements);
@@ -88,7 +88,7 @@ void initGraphics()
 	triangleVAO.bufferData((float*)triangle, sizeof(triangle));
 	triangleVAO.setVertexSpecification(&spec);
 
-	modelChair = new Model(std::filesystem::path("Resources\\Models\\old_wooden_chair\\old_wooden_chair.fbx"));
+	//modelChair = new Model(std::filesystem::path("Resources\\Models\\old_wooden_chair\\old_wooden_chair.fbx"));
 
 	camera = new Camera(0.0f, 0.0f, -6.0f, 0.0f, 1.0f, 0.0f, 90.0f, 0.0f);
 }
@@ -97,29 +97,30 @@ void initShaders()
 {
 	std::vector<VertexElement> elements =
 	{
-		VertexElement{4, GL_FLOAT, false, ElementType::XYZW },
+		VertexElement{3, GL_FLOAT, false, ElementType::XYZ  },
 		VertexElement{3, GL_FLOAT, false, ElementType::COLOR}
 	};
 	const VertexSpecification* spec = VertexSpecificationManager::instance().CreateVertexSpecification(elements);
 	shaderTranslated = new Shader(shader_names::triangle_vs, shader_names::triangle_ps, spec);
 
-	shaderChair = new Shader(shader_names::chair_vs, shader_names::chair_ps, NULL);
+	//shaderChair = new Shader(shader_names::chair_vs, shader_names::chair_ps, NULL);
 }
 
 void render()
 {
-	glClearColor(0.2, 0.0, 0.2, 1.0);
+	glClearColor(0.4, 0.0, 0.2, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glm::mat4 model = glm::scale(glm::mat4(1.0f), glm::vec3(0.01, 0.01, 0.01));// glm::mat4(1.0f);//
-	glm::mat4 view = camera->GetViewMatrix();
-	glm::mat4 proj = glm::perspective(glm::radians(60.0f), (float)800 / (float)600, 0.1f, 100.0f);
-	shaderChair->setMat4f("model", model);
-	shaderChair->setMat4f("view", view);
-	shaderChair->setMat4f("proj", proj);
-	shaderChair->use();
+	//glm::mat4 model = glm::scale(glm::mat4(1.0f), glm::vec3(0.01, 0.01, 0.01));// glm::mat4(1.0f);//
+	//glm::mat4 view = camera->GetViewMatrix();
+	//glm::mat4 proj = glm::perspective(glm::radians(60.0f), (float)800 / (float)600, 0.1f, 100.0f);
+	//shaderChair->setMat4f("model", model);
+	//shaderChair->setMat4f("view", view);
+	//shaderChair->setMat4f("proj", proj);
+	//shaderChair->use();
+	shaderTranslated->use();
 	triangleVAO.bind();
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 	triangleVAO.unbind();
 	//shaderChair->use();
-	modelChair->Draw(*shaderChair);
+	//modelChair->Draw(*shaderChair);
 }
